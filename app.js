7386e5c4ecbd64d7b34ab543bc7afd6042187065
niclasmas <massanicolas.nm@gmail.com>
2025-08-19 23:43:13 -0300

    Sorte amigos Final

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const inputNombre = document.getElementById('amigo');
+    const nombreIngresado = inputNombre.value;
+    
+    if (nombreIngresado === '' || nombreIngresado.trim() === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+    
+    amigos.push(nombreIngresado.trim());
+    inputNombre.value = '';
+    
+    mostrarAmigosEnLista();
+    
+    console.log('Amigo agregado:', nombreIngresado.trim());
+    console.log('Lista actual de amigos:', amigos);
+}
+
+function mostrarAmigosEnLista() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    
+    for (let i = 0; i < amigos.length; i++) {
+        const nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = amigos[i];
+        lista.appendChild(nuevoElemento);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Por favor, añade al menos un amigo antes de sortear.');
+        return;
+    }
+    
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    
+    const elementoResultado = document.getElementById('resultado');
+    elementoResultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+    
+    console.log('Índice aleatorio generado:', indiceAleatorio);
+    console.log('Amigo sorteado:', amigoSorteado);
+}
